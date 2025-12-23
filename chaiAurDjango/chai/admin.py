fab12337f88d52be86346225b70da0a135b8a06d
sibgatullah-dev <sibgatullah881@gmail.com>
2025-12-23 09:07:09 +0600

    removed previous files and fresh start for review

--- chaiAurDjango/chai/admin.py
+++
@@ -1,6 +0,0 @@
-# with admin.py we can attach any models and see in admin 
-from django.contrib import admin
-from .models import ChaiVaraity #importing the model (ChaiVarity) from models.py
-
-# Register your models here.
-admin.site.register(ChaiVaraity)
--- chaiAurDjango/chai/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from .import views
-
-urlpatterns = [
-   path('',views.allChai,name='allChai'),
-   path('<int:chai_id>/', views.chai_details, name='chai_details'),
-   
-]
