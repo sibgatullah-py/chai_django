79b547ed38fa2d8eba5c049b868758e50d242cd4
sibgatullah-dev <sibgatullah881@gmail.com>
2025-12-23 19:25:21 +0600

    templates and static

--- chaiaurDjango/chaiaurDjango/views.py
+++ chaiaurDjango/chaiaurDjango/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render 
  
 def home(request):
-    return HttpResponse("Hello, world. You are at chai aur Django Home page")
+    # return HttpResponse("Hello, world. You are at chai aur Django Home page")
+    return render(request, 'website/index.html')
 
 def about(request):
-    return HttpResponse("Hello, world. You are at chai aur Django About page")
+    return render(request, 'website/about.html')
 
 def contact(request):
-    return HttpResponse("Hello, world. You are at chai aur Django Contact page")
+    return render(request, 'website/contact.html')
