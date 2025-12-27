348e4ed4f3f2167e9b58c0be44a5adc4c86c46f4
sibgatullah-dev <sibgatullah881@gmail.com>
2025-12-27 14:25:13 +0600

    made a chai page design

--- chaiaurDjango/chai/views.py
+++ chaiaurDjango/chai/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import ChaiVarity
 
 # Create your views here.
 
 def all_chai(request):
-    return render(request, 'chai/all_chai.html')
+    chais = ChaiVarity.objects.all() # taking all the models of ChaiVarity in a single variable for use. This will return an Array of models 
+    return render(request, 'chai/all_chai.html', {'chais':chais}) # This carly braces sends values in the frontend in form of dictionary
