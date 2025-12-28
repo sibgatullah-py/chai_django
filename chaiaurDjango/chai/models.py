ab9d0b11c5d10881d9e2347cedd42657a73858c4
sibgatullah-dev <sibgatullah881@gmail.com>
2025-12-28 15:02:07 +0600

    ORM

--- chaiaurDjango/chai/models.py
+++ chaiaurDjango/chai/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User # default user model form django 
 
 # Create your models here.
 class ChaiVarity(models.Model):
@@ -28,3 +29,32 @@ class ChaiVarity(models.Model):
 
     def __str__(self): # This dunder method is returning the name of the item in the database 
         return self.name
+    
+# One to Many
+class ChaiReview(models.Model):
+    chai = models.ForeignKey(ChaiVarity, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    rating = models.ImageField()
+    comment = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return f'{self.user.username} review for {self.chai.name}'
+    
+# Many to Many
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=200)
+    chai_varieties = models.ManyToManyField(ChaiVarity, related_name='stores')
+    
+    def __str__(self):
+        return self.name
+    
+# One to One 
+class ChaiCertificate(models.Model):
+    chai = models.OneToOneField(ChaiVarity, on_delete=models.CASCADE, related_name='certificate')
+    certificate_number = models.CharField(max_length=100)
+    issue_date = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return f'Certificate for {self.name.chai}'
